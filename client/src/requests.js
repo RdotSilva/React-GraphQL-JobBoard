f9bea07f480757b661e60e94cdf8142ad18beb03
RdotSilva <RyandotSilva@gmail.com>
2021-06-28 13:17:31 -0400

    Refactor loadJobsQuery

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,20 +1,22 @@
 const endpointURL = "http://localhost:9000/graphql";
 
+const loadJobsQuery = `{
+  jobs {
+      id
+      title
+      company {
+          id
+          name
+      }
+  }
+}`;
+
 export const loadJobs = async () => {
   const response = await fetch(endpointURL, {
     method: "POST",
     headers: { "content-type": "application/json" },
     body: JSON.stringify({
-      query: `{
-            jobs {
-                id
-                title
-                company {
-                    id
-                    name
-                }
-            }
-        }`,
+      query: loadJobsQuery,
     }),
   });
 

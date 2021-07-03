05888002a5a7967e05db6b4ed9c2593be6ca245c
RdotSilva <RyandotSilva@gmail.com>
2021-07-03 15:17:08 -0400

    Add resolver to handle mutations

--- server/resolvers.js
+++ server/resolvers.js
@@ -7,6 +7,12 @@ const Query = {
   roles: () => db.roles.lis(),
 };
 
+const Mutation = {
+  createJob: (root, { companyId, title, description }) => {
+    return db.jobs.create({ companyId, title, description });
+  },
+};
+
 const Company = {
   jobs: (company) =>
     db.jobs.list().filter((job) => job.companyId === company.id),
@@ -16,4 +22,4 @@ const Job = {
   company: (job) => db.companies.get(job.companyId),
 };
 
-module.exports = { Query, Job, Company };
+module.exports = { Query, Mutation, Job, Company };

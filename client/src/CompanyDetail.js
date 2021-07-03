545143e1468a175b507a6d4c806ec7aac5ef8530
RdotSilva <RyandotSilva@gmail.com>
2021-07-03 15:09:09 -0400

    Add logic to render job list for company

--- client/src/CompanyDetail.js
+++ client/src/CompanyDetail.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { loadCompany } from "./requests";
+import { JobList } from "./JobList";
 
 export const CompanyDetail = ({ match }) => {
   const [company, setCompany] = useState(null);
@@ -9,7 +10,7 @@ export const CompanyDetail = ({ match }) => {
     loadCompany(companyId).then((data) => {
       setCompany(data);
     });
-  }, []);
+  }, [match.params]);
 
   return !company ? (
     <div>loading</div>
@@ -17,6 +18,8 @@ export const CompanyDetail = ({ match }) => {
     <div>
       <h1 className="title">{company.name}</h1>
       <div className="box">{company.description}</div>
+      <h5 className="title is-5">Jobs at {company.name}</h5>
+      <JobList jobs={company.jobs} />
     </div>
   );
 };

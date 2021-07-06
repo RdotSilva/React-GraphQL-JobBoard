0c3c61461c6dbcf2a076973f90355777d1d16bda
RdotSilva <RyandotSilva@gmail.com>
2021-07-06 15:47:20 -0400

    Refactor state

--- client/src/JobForm.js
+++ client/src/JobForm.js
@@ -1,21 +1,24 @@
 import React, { useState } from "react";
 import { createJob } from "./requests";
 
-export const JobForm = () => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+export const JobForm = (props) => {
+  const [state, setState] = useState({
+    title: "",
+    description: "",
+  });
 
   const handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
+    const value = event.target.value;
+
+    setState({ ...state, [event.target.name]: value });
   };
 
   const handleClick = (event) => {
     event.preventDefault();
     const companyId = "SJV0-wdOM"; // Temporary hard coded
-    const { title, description } = this.state;
+    const { title, description } = state;
     createJob({ companyId, title, description }).then((job) => {
-      this.props.history.push(`/jobs/${job.id}`);
+      props.history.push(`/jobs/${job.id}`);
     });
   };
 
@@ -31,7 +34,7 @@ export const JobForm = () => {
                 className="input"
                 type="text"
                 name="title"
-                value={title}
+                value={state.title}
                 onChange={handleChange}
               />
             </div>
@@ -43,14 +46,14 @@ export const JobForm = () => {
                 className="input"
                 style={{ height: "10em" }}
                 name="description"
-                value={description}
+                value={state.description}
                 onChange={handleChange}
               />
             </div>
           </div>
           <div className="field">
             <div className="control">
-              <button className="button is-link" onClick={handleChange}>
+              <button className="button is-link" onClick={handleClick}>
                 Submit
               </button>
             </div>

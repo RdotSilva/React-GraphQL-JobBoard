c4a581d91e8e1abc802220db80b2f2f83310ba1d
RdotSilva <RyandotSilva@gmail.com>
2021-07-12 13:30:13 -0400

    Add logic to load all roles

--- client/src/RoleList.js
+++ client/src/RoleList.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { loadRoles } from "./requests";
 
-export const RoleList = ({ roles }) => {
+export const RoleList = () => {
+  const [roles, setRoles] = useState([]);
+
+  useEffect(() => {
+    loadRoles().then((data) => {
+      setRoles(data);
+    });
+  }, []);
+
   const renderRole = (role) => {
     const title = role.company
       ? `${role.title} at ${role.company.name}`

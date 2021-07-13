e533b87430103f9fd237c0a84c0592d25a15f132
RdotSilva <RyandotSilva@gmail.com>
2021-07-13 14:19:15 -0400

    Add role display for company

--- client/src/CompanyDetail.js
+++ client/src/CompanyDetail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { loadCompany } from "./requests";
 import { JobList } from "./JobList";
+import { RoleList } from "./RoleList";
 
 export const CompanyDetail = ({ match }) => {
   const [company, setCompany] = useState(null);
@@ -20,6 +21,8 @@ export const CompanyDetail = ({ match }) => {
       <div className="box">{company.description}</div>
       <h5 className="title is-5">Jobs at {company.name}</h5>
       <JobList jobs={company.jobs} />
+      <h5 className="title is-5">Roles at {company.name}</h5>
+      <RoleList />
     </div>
   );
 };

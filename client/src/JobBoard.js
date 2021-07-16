ec16a3703c9acc09669c3514b769bd64eda2c21a
RdotSilva <RyandotSilva@gmail.com>
2021-07-16 13:04:22 -0400

    Add Toggle to filter jobs

--- client/src/JobBoard.js
+++ client/src/JobBoard.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { JobList } from "./JobList";
 import { loadHiringJobs } from "./requests";
+import Toggle from "react-toggle";
 
 export const JobBoard = () => {
   const [jobs, setJobs] = useState([]);
+  const [showAllJobs, setShowAllJobs] = useState(false);
 
   useEffect(() => {
     loadHiringJobs().then((data) => {
@@ -14,6 +16,12 @@ export const JobBoard = () => {
   return (
     <div>
       <h1 className="title">Job Board</h1>
+      <Toggle
+        checked={showAllJobs}
+        name="jobToggle"
+        value={showAllJobs}
+        onChange={() => setShowAllJobs(!showAllJobs)}
+      />
       <JobList jobs={jobs} />
       // TODO: Filter out jobs that are not hiring
     </div>
